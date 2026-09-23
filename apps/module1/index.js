4ad5da026c44d4952bfe9b17f071e2faf000543c
Minh Pham <minh.pham@alphanetworks.global>
2026-09-23 15:52:37 +0700

    [Feat/new-alpha1] add alpha2 function and invoke it in module export

--- apps/module1/index.js
+++ apps/module1/index.js
@@ -6,6 +6,9 @@ function alpha() {
 function alpha1() {
   console.log('Alpha1 function called');
 }
+function alpha2() {
+  console.log('Alpha2 function called');
+}
 function betaUpdated() {
   console.log('Beta function called');
 }
@@ -13,5 +16,6 @@ module.exports = function () {
   console.log('Module 1 loaded');
   alpha();
   alpha1();
+  alpha2();
   betaUpdated();
 };
