f9e9fe49512a8bdf7b2a9ccfe9357b6d35dfd7a6
Minh Pham <minh.pham@alphanetworks.global>
2026-09-23 15:02:04 +0700

    [Feat/alpha] add beta function to module1 and invoke it on load

--- apps/module1/index.js
+++ apps/module1/index.js
@@ -3,7 +3,11 @@
 function alpha() {
   console.log('Alpha function called');
 }
+function beta() {
+  console.log('Beta function called');
+}
 module.exports = function () {
   console.log('Module 1 loaded');
   alpha();
+  beta();
 };
