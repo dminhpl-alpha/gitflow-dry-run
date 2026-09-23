ba94cb49b6a3866044a7fc8d107d27b27ae9b8ba
Minh Pham <minh.pham@alphanetworks.global>
2026-09-23 15:05:55 +0700

    [Feat/Beta]: rename beta function to betaUpdated for clarity

--- apps/module1/index.js
+++ apps/module1/index.js
@@ -3,11 +3,11 @@
 function alpha() {
   console.log('Alpha function called');
 }
-function beta() {
+function betaUpdated() {
   console.log('Beta function called');
 }
 module.exports = function () {
   console.log('Module 1 loaded');
   alpha();
-  beta();
+  betaUpdated();
 };
